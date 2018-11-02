61c969f0c5d2cae83519b55125f3c82b59fa13ad
Thauvin Alexandre <alexandre.thauvin@epitech.eu>
2018-11-02 20:59:10 +0800

    Fragment created + home fragment completed

--- app/src/main/java/alexandre/thauvin/gym3000x/MainActivity.java
+++ app/src/main/java/alexandre/thauvin/gym3000x/MainActivity.java
@@ -1,7 +1,12 @@
 package alexandre.thauvin.gym3000x;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AHBottomNavigation bottomNavigation = findViewById(R.id.bottom_navigation);
+        AHBottomNavigationItem item1 = new AHBottomNavigationItem(R.string.dashboard, R.drawable.baseline_dashboard_black_24dp, R.color.colorAccent);
+        AHBottomNavigationItem item2 = new AHBottomNavigationItem(R.string.courses, R.drawable.baseline_account_balance_black_24dp, R.color.colorAccent);
+        AHBottomNavigationItem item3 = new AHBottomNavigationItem(R.string.trainers, R.drawable.baseline_accessibility_black_24dp, R.color.colorAccent);
+
+        bottomNavigation.addItem(item1);
+        bottomNavigation.addItem(item2);
+        bottomNavigation.addItem(item3);
+        bottomNavigation.setDefaultBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+        bottomNavigation.setForceTint(true);
+        bottomNavigation.setCurrentItem(0);
+
+        bottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
+            @Override
+            public boolean onTabSelected(int position, boolean wasSelected) {
+                switch (position){
+                    case 0:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, new HomeFragment()).commit();
+                        break;
+                    case 1:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, new CoursesFragment()).commit();
+                        break;
+                    case 2:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, new TrainersFragment()).commit();
+                        break;
+                    default :
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, new HomeFragment()).commit();
+
+                }
+                return true;
+            }
+        });
     }
 }
